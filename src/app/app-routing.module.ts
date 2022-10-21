ebb390ab1ef71d5a499dccbeb2eba300c665d203
George Kakarlis <georgekakarlis@yahoo.com>
2022-10-21 02:27:34 +0200

    minor fixes commited. Incoming Layout fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SigninComponent } from './auth/signin/signin.component';
 import { DummyTextComponent } from './dummy-text/dummy-text.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 
 const routes: Routes = [
   {path: 'home',component:DummyTextComponent},
-  {path: 'products',component:DummyTextComponent},
-  {path: 'aboutus',component:DummyTextComponent},
-  {path: 'contactus',component:DummyTextComponent},
+  {path: 'pricing',component:DummyTextComponent},
+  {path: 'features',component:DummyTextComponent},
   {path: 'signup',component:DummyTextComponent},
-  {path: 'signin',component:DummyTextComponent},
+  {path: 'signin',component:SigninComponent},
+     //Wild Card Route for 404 request
+     { path: '**', pathMatch: 'full',
+     component: PageNotFoundComponent },
 ];
 
 @NgModule({
